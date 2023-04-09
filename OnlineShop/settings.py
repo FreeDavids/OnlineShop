9158167962d286cb7315a5fd798d9ad905c4b114
David Martin Sanchez <martinsanchezdavid27@gmail.com>
2023-04-09 03:25:12 +0200

    Security

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -148,8 +148,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "martinsanchezdavid27@gmail.com"
-EMAIL_HOST_PASSWORD = "Davilongo931520"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 
 
 # Error messages settings
